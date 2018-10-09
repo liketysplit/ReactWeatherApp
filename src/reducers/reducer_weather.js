9b511219fb4e93e6f7c1e816581099f3ff617c02
liketysplit <rjboles2@gmail.com>
2018-10-09 18:34:04 -0400

    Add: Added averages to the state to use in sorting as suggested

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,9 +1,37 @@
 import { FETCH_WEATHER } from "../actions/index";
+import _ from "lodash";
+
 export default function(state = [], action) {
-	console.log("Action received (post middleware):", action);
+	// console.log("Action received (post middleware):", action);
 	switch (action.type) {
 		case FETCH_WEATHER:
-			return [action.payload.data, ...state];
+			//Get Data
+			let temp_temperatures = action.payload.data.list.map(
+				weather => weather.main.temp
+			);
+
+			let temp_pressures = action.payload.data.list.map(
+				weather => weather.main.pressure
+			);
+
+			let temp_humidites = action.payload.data.list.map(
+				weather => weather.main.humidity
+			);
+
+			//Get Averages with Lodash
+			let avg_temperature = _.round(_.meanBy(temp_temperatures));
+			let avg_pressure = _.round(_.meanBy(temp_pressures));
+			let avg_humidity = _.round(_.meanBy(temp_humidites));
+
+			//Return State
+			return [
+				Object.assign(action.payload.data, {
+					avg_temperature,
+					avg_pressure,
+					avg_humidity
+				}),
+				...state
+			];
 	}
 	return state;
 }
